f50df82fef18358696fbab4ddd50035a192e85dc
Michael Macaulay <56690114+MichaelMacaulay@users.noreply.github.com>
2021-10-27 13:17:07 -0400

    Cleaning up smart contract and adding chainlink's RVF for secured randomness

--- contracts/EtherCoinFlip.sol
+++ contracts/EtherCoinFlip.sol
@@ -1,8 +1,36 @@
 // SPDX-License-Identifier: MIT
 
-pragma solidity ^0.7.6;
+pragma solidity ^0.8.0;
 
-contract EtherCoinFlip {
+import "@chainlink/contracts/src/v0.8/VRFConsumerBase.sol";
+
+contract EtherCoinFlip is VRFConsumerBase {
+    // testing with hard-coded values
+    constructor()
+        VRFConsumerBase(
+            0xdD3782915140c8f3b190B5D67eAc6dc5760C46E9, // VRF Coordinator
+            0xa36085F69e2889c224210F603D836748e7dC0088 // LINK Token
+        )
+    {
+        keyHash = 0x6c3699283bda56ad74f6b855546325b68d482e983852a7a82979cc4807b641f4;
+        fee = 0.1 * 10**18; // 0.1 LINK (Varies by network)
+    }
+
+    function getRandomNumber() public returns (bytes32 requestId) {
+        require(
+            LINK.balanceOf(address(this)) >= fee,
+            "Not enough LINK - fill contract with faucet"
+        );
+        return requestRandomness(keyHash, fee);
+    }
+
+    function fulfillRandomness(bytes32 requestId, uint256 randomness)
+        internal
+        override
+    {
+        randomResult = randomness;
+    }
+
     struct EtherCoinFlipStruct {
         uint256 ID;
         address payable betStarter;
@@ -14,6 +42,10 @@ contract EtherCoinFlip {
         address payable loser;
     }
 
+    bytes32 internal keyHash;
+    uint256 internal fee;
+    uint256 public randomResult;
+
     uint256 numCoinFlips = 300;
     mapping(uint256 => EtherCoinFlipStruct) EtherCoinFlipStructs;
 
@@ -21,16 +53,18 @@ contract EtherCoinFlip {
 
     // Start the Ether coin flip
     function newCoinFlip() public payable returns (uint256 coinFlipID) {
+        address theBetStarter = msg.sender;
+        address payable player1 = payable(theBetStarter);
         coinFlipID = numCoinFlips++;
         EtherCoinFlipStructs[coinFlipID] = EtherCoinFlipStruct(
             coinFlipID,
-            msg.sender,
+            player1,
             msg.value,
-            msg.sender,
+            player1,
             msg.value,
             0,
-            msg.sender,
-            msg.sender
+            player1,
+            player1
         );
         emit EtherCoinFlipped(coinFlipID);
     }
@@ -41,22 +75,19 @@ contract EtherCoinFlip {
     function endCoinFlip(uint256 coinFlipID) public payable {
         EtherCoinFlipStruct memory c = EtherCoinFlipStructs[coinFlipID];
 
+        address theBetender = msg.sender;
+        address payable player2 = payable(theBetender);
+
         require(coinFlipID == c.ID);
         require(msg.value == c.startingWager);
 
-        c.betEnder = msg.sender;
+        c.betEnder = player2;
         c.endingWager = msg.value;
         c.etherTotal = c.startingWager + c.endingWager;
 
-        uint256 finalVerdict = block.number +
-            block.timestamp +
-            msg.value +
-            block.difficulty +
-            block.gaslimit +
-            tx.gasprice +
-            1;
+        fulfillRandomness(getRandomNumber(), coinFlipID);
 
-        if ((finalVerdict % 2) == 0) {
+        if ((randomResult % 2) == 0) {
             c.winner = c.betStarter;
         } else {
             c.winner = c.betEnder;
