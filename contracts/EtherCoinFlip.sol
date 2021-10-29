bc8cc8d9202e0d5c8e37a8c6d944d491c820b9ee
Michael Macaulay <56690114+MichaelMacaulay@users.noreply.github.com>
2021-10-29 11:20:44 -0400

    dependencies & constructor function fixes

--- contracts/EtherCoinFlip.sol
+++ contracts/EtherCoinFlip.sol
@@ -2,18 +2,18 @@
 
 pragma solidity ^0.8.0;
 
-import "@chainlink/contracts/src/v0.8/VRFConsumerBase.sol";
+// import "@chainlink/contracts/src/v0.8/VRFConsumerBase.sol";
 
 contract EtherCoinFlip is VRFConsumerBase {
     // testing with hard-coded values
     constructor()
         VRFConsumerBase(
-            0xdD3782915140c8f3b190B5D67eAc6dc5760C46E9, // VRF Coordinator
-            0xa36085F69e2889c224210F603D836748e7dC0088 // LINK Token
+            0xb3dCcb4Cf7a26f6cf6B120Cf5A73875B7BBc655B, // VRF Coordinator
+            0x01BE23585060835E02B77ef475b0Cc51aA1e0709 // LINK Token
         )
     {
-        keyHash = 0x6c3699283bda56ad74f6b855546325b68d482e983852a7a82979cc4807b641f4;
-        fee = 0.1 * 10**18; // 0.1 LINK (Varies by network)
+        keyHash = 0x2ed0feb3e7fd2022120aa84fab1945545a9f2ffc9076fd6156fa96eaff4c1311;
+        fee = 0.1;
     }
 
     function getRandomNumber() public returns (bytes32 requestId) {
@@ -74,7 +74,6 @@ contract EtherCoinFlip is VRFConsumerBase {
     // End the Ether coin flip
     function endCoinFlip(uint256 coinFlipID) public payable {
         EtherCoinFlipStruct memory c = EtherCoinFlipStructs[coinFlipID];
-
         address theBetender = msg.sender;
         address payable player2 = payable(theBetender);
 
