0bcf109c215ef0261adafe6073491a3d3beb6682
Michael Macaulay <56690114+MichaelMacaulay@users.noreply.github.com>
2022-12-18 11:41:32 -0500

    Rewriting the smart contract

--- contracts/EtherCoinFlip.sol
+++ contracts/EtherCoinFlip.sol
@@ -1,35 +1,7 @@
 // SPDX-License-Identifier: MIT
+pragma solidity ^0.8.17;
 
-pragma solidity ^0.8.0;
-
-import "@chainlink/contracts/src/v0.8/VRFConsumerBase.sol";
-
-contract EtherCoinFlip is VRFConsumerBase {
-    // testing with hard-coded values
-    constructor()
-        VRFConsumerBase(
-            0xb3dCcb4Cf7a26f6cf6B120Cf5A73875B7BBc655B, // VRF Coordinator
-            0x01BE23585060835E02B77ef475b0Cc51aA1e0709 // LINK Token
-        )
-    {
-        keyHash = 0x2ed0feb3e7fd2022120aa84fab1945545a9f2ffc9076fd6156fa96eaff4c1311;
-        fee = 0.1 * 10**18;
-    }
-
-    function getRandomNumber() public returns (bytes32 requestId) {
-        require(
-            LINK.balanceOf(address(this)) >= fee,
-            "Not enough LINK - fill contract with faucet"
-        );
-        return requestRandomness(keyHash, fee);
-    }
-
-    function fulfillRandomness(bytes32 requestId, uint256 randomness)
-        internal
-        override
-    {
-        randomResult = randomness;
-    }
+contract EtherCoinFlip {
 
     struct EtherCoinFlipStruct {
         uint256 ID;
@@ -42,14 +14,10 @@ contract EtherCoinFlip is VRFConsumerBase {
         address payable loser;
     }
 
-    bytes32 internal keyHash;
-    uint256 internal fee;
-    uint256 public randomResult;
-
     uint256 numCoinFlips = 300;
-    mapping(uint256 => EtherCoinFlipStruct) EtherCoinFlipStructs;
+    mapping(uint256 => EtherCoinFlipStruct) public EtherCoinFlipStructs;
 
-    event EtherCoinFlipped(uint256 indexed theCoinFlipID);
+    event startedCoinfFlip(uint256 indexed theCoinFlipID);
 
     // Start the Ether coin flip
     function newCoinFlip() public payable returns (uint256 coinFlipID) {
@@ -66,25 +34,25 @@ contract EtherCoinFlip is VRFConsumerBase {
             player1,
             player1
         );
-        emit EtherCoinFlipped(coinFlipID);
+        emit startedCoinfFlip(coinFlipID);
     }
 
-    event EtherCoinFinishedFlip(address indexed winner);
+    event finishedCoinFlip(address indexed winner);
 
-    // End the Ether coin flip
+    // End the coin flip
     function endCoinFlip(uint256 coinFlipID) public payable {
-        EtherCoinFlipStruct memory c = EtherCoinFlipStructs[coinFlipID];
+        EtherCoinFlipStruct storage c = EtherCoinFlipStructs[coinFlipID];
         address theBetender = msg.sender;
         address payable player2 = payable(theBetender);
 
-        require(coinFlipID == c.ID);
-        require(msg.value == c.startingWager);
+        require(coinFlipID == c.ID, "Invalid coin flip ID");
+        require(msg.value == c.startingWager, "Invalid wager amount");
 
         c.betEnder = player2;
         c.endingWager = msg.value;
         c.etherTotal = c.startingWager + c.endingWager;
 
-        fulfillRandomness(getRandomNumber(), coinFlipID);
+        uint256 randomResult = block.difficulty + block.chainid + block.gaslimit + block.number + block.timestamp;
 
         if ((randomResult % 2) == 0) {
             c.winner = c.betStarter;
@@ -93,6 +61,6 @@ contract EtherCoinFlip is VRFConsumerBase {
         }
 
         c.winner.transfer(c.etherTotal);
-        emit EtherCoinFinishedFlip(c.winner);
+        emit finishedCoinFlip(c.winner);
     }
 }
