c02ecd0b79bb64ea071f86900eebfa2e901a9761
Micmac.eth <56690114+MichaelMacaulay@users.noreply.github.com>
2023-03-03 13:17:35 -0500

    refactoring in _app.js

--- my-app/pages/_app.js
+++ my-app/pages/_app.js
@@ -13,10 +13,15 @@ import { publicProvider } from 'wagmi/providers/public';
 import { ConnectButton } from '@rainbow-me/rainbowkit';
 import '../styles/dashboard.css';
 import '../styles/button.css';
+import ethercoinflip from '../utils/ethercoinflip.json';
+import { ethers } from "ethers";
+import { useContractWrite, usePrepareContractWrite } from 'wagmi'
 
 export const YourApp = () => {
   return <ConnectButton />;
 };
+
+const contractAddress = "0x575fE957730F8Db4635A405daEad4B89544A5907";
 
 const { chains, provider } = configureChains(
   [goerli],
@@ -25,6 +30,19 @@ const { chains, provider } = configureChains(
     publicProvider()
   ]
 );
+
+
+  const { config } = usePrepareContractWrite({
+    address: '0x575fE957730F8Db4635A405daEad4B89544A5907',
+    abi: ethercoinflip,
+    functionName: 'newCoinFlip',
+  })
+
+    const { data, isLoading, isSuccess, write } = useContractWrite(config)
+
+
+
+
 const { connectors } = getDefaultWallets({
   appName: 'My RainbowKit App',
   chains
@@ -42,13 +60,30 @@ const client = createUrqlClient({
 });
 
 function MyApp({ Component, pageProps }) {
+
+    const { state: transactionState, send } = useContractFunction(EtherCoinFlipContract, "newCoinFlip");
+  
+  async function handleFlipCoin(amount) {
+    const value = ethers.utils.parseEther(amount); // convert the amount to wei
+    await send({ value });
+  }
+
   return (
     <WagmiConfig client={wagmiClient}>
       <RainbowKitProvider chains={chains}>
         <Provider value={client}>
-          <div className="flex justify-center mt-20" >  {/* styled with Tailwind (will add tailwind) */}
+          <div>
             <ConnectButton />
           </div>
+          <div>
+      <button onClick={() => handleFlipCoin("0.1")}>Flip Coin for 0.1 Ether</button>
+      {transactionState && (
+        <div>
+          <p>Transaction Hash: {transactionState.transactionHash}</p>
+          <p>Status: {transactionState.status}</p>
+        </div>
+      )}
+    </div>
         <Component {...pageProps} />
       </Provider>
       </RainbowKitProvider>
--- my-app/pages/index.js
+++ my-app/pages/index.js
@@ -1,12 +1,10 @@
 import Dashboard from '../components/dashboard.js';
-import StartCoinFlip from '../components/startcoinflip.js';
 
 export default function Home() {
 
 
   return (
     <div>
-      <StartCoinFlip/>
         <Dashboard/>
     </div>
   );
